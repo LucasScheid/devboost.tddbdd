35aa90977a6fa087dd4d03abc0fe4cd390422249
Lucas Scheid <lucas.scheid@penske.com>
2020-09-03 21:26:58 -0300

    Initial commit.

---
+++ Services/Security/ValidateDatabse.cs
@@ -0,0 +1,20 @@
+﻿using devboost.dronedelivery.felipe.EF.Data;
+using devboost.dronedelivery.felipe.Security.Interfaces;
+using System.Diagnostics.CodeAnalysis;
+
+namespace devboost.dronedelivery.felipe.Security
+{
+    [ExcludeFromCodeCoverage]
+    public class ValidateDatabse : IValidateDatabase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public ValidateDatabse(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public bool EnsureCreated()
+        {
+            return _applicationDbContext.Database.EnsureCreated();
+        }
+    }
+}
